6c69575547e46677ad65570e155c6bbf6fb720af
Sritejaswini123 <sritejaswini1234@gmail.com>
2024-02-20 13:33:06 +0530

    IMGINSERCTION

---
+++ Newimageproject/src/Components/Components2/index.jsx
@@ -0,0 +1,14 @@
+// Components2.jsx
+import './index.css'
+const Components2 = () => {
+  return (
+    <div className="components2-container">
+      <h1 style={{  fontFamily: 'Roboto' }}>Super over league</h1>
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"  className="logo" />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png"  className="logo" />
+    </div>
+  );
+};
+export default Components2;
+// alt="CSK Logo"
+//alt="RCB Logo"
